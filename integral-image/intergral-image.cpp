b4efaf5e9a8b1b10286a7f706aef0c4640a69432
Sy Tran <sytrancsvn@gmail.com>
2021-01-31 23:39:13 +0700

    Integral image

--- integral-image/intergral-image.cpp
+++ integral-image/intergral-image.cpp
@@ -3,7 +3,7 @@
 #include <utility>
 
 typedef std::pair<size_t, size_t> Pos;
-void print_arr2(int** arr, int row, int col);
+void print_arr2(int **arr, int row, int col);
 class IntergralImage
 {
 	size_t m_x, m_y;
@@ -15,11 +15,11 @@ public:
 	{
 		m_x = x;
 		m_y = y;
-		m_ii = (int**)malloc(m_x * sizeof(int*)); // init m_x * m_y grid
+		m_ii = (int **)malloc(m_x * sizeof(int *)); // init m_x * m_y grid
 
 		for (int i = 0; i < m_x; i++)
 		{
-			m_ii[i] = (int*)calloc(0, m_y * sizeof(int)); // set all to zeros
+			m_ii[i] = (int *)calloc(0, m_y * sizeof(int)); // set all to zeros
 		}
 
 		for (int i = 0; i < m_x; i++)
@@ -30,41 +30,41 @@ public:
 				// s(x,y) = s(x,y-1) + img(x,y)
 				// ii(x,y) = ii(x-1, y) + s(x,y)
 				s += img[i][j]; // we dont have extra row and column in original image
-				m_ii[i][j] = (i > 0 ? m_ii[i - 1][j] : 0 ) + s;
+				m_ii[i][j] = (i > 0 ? m_ii[i - 1][j] : 0) + s;
 			}
 		}
 	};
 
-	int** ii() {
+	int **ii()
+	{
 		return m_ii;
 	}
 
-	void print_ii() {
+	void print_ii()
+	{
 		print_arr2(m_ii, m_x, m_y);
 	}
 
 	/**
 	 * get actual sum of <Top, Left>, <Bottom, Right> rectangle
 	 */
-	int s_at(Pos tl, Pos br)
+	int sum(Pos tl, Pos br)
 	{
-		if (tl.first < 0 || tl.second < 0 ||  br.first > m_x - 1 || br.second > m_y - 1)
-		{
-			perror("Index out of range");
-			return -1;
-		}
-		int a = m_ii[tl.first + 1][tl.second + 1];
-		int b = m_ii[tl.first + 1][br.second + 1];
-		int c = m_ii[br.first + 1][tl.second + 1];
-		int d = m_ii[br.first + 1][br.second + 1];
+		int a = tl.first && tl.second ? m_ii[tl.first - 1][tl.second - 1] : 0;
+		int b = tl.first ? m_ii[tl.first - 1][br.second] : 0;
+		int c = tl.second ? m_ii[br.first][tl.second - 1] : 0;
+		int d = m_ii[br.first][br.second];
+
 		return d + a - (b + c);
 	}
 
 	~IntergralImage() {}
 };
 
-void print_arr2(int** arr, int row, int col) {
-	for (int i = 0; i < row; i++) {
+void print_arr2(int **arr, int row, int col)
+{
+	for (int i = 0; i < row; i++)
+	{
 		int j;
 		for (j = 0; j < col - 1; j++)
 			printf("%d ", arr[i][j]);
--- integral-image/main.cpp
+++ integral-image/main.cpp
@@ -1,24 +1,36 @@
 #include "intergral-image.cpp"
 
-int main() {
+int main()
+{
 	int row, col;
 
 	scanf("%d %d", &row, &col);
 
 	int **arr = (int **)malloc(row * sizeof(int *));
-	for (int i = 0; i < row; i++) {
+	for (int i = 0; i < row; i++)
+	{
 		arr[i] = (int *)malloc(col * sizeof(int));
 	}
 
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
 			scanf("%d", &arr[i][j]);
 		}
 	}
 
-	IntergralImage* ii = new IntergralImage(arr, row, col);
+	IntergralImage *ii = new IntergralImage(arr, row, col);
 
 	ii->print_ii();
+
+	printf("sum {0,0}, {2,2}: %d\n", ii->sum({0, 0}, {2, 2}));
+	printf("sum {1,1}, {2,2}: %d\n", ii->sum({1, 1}, {2, 2}));
+	printf("sum {1,1}, {3,3}: %d\n", ii->sum({1, 1}, {3, 3}));
+	printf("sum {%d,%d}, {%d,%d}: %d\n",
+		 row - 1, col - 1,
+		 row - 1, col - 1,
+		 ii->sum({row - 1, col - 1}, {row - 1, col - 1}));
 
 	return 0;
 }
